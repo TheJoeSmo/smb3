9c2ad344a484b908d169ae81e0cd6df9f93aa4b5
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-12-19 17:40:43 -0500

    Refactor star_firework into seperate file

---
+++ src/gfx_effects/video_updates/star_firework.asm
@@ -0,0 +1,10 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; JoeSmo 12/19/2020
+; The video updates for the star fireworks
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    .include src/gfx_effects/video_updates/star_firework/top.asm
+    .include src/gfx_effects/video_updates/star_firework/tip.asm
+    .include src/gfx_effects/video_updates/star_firework/sides.asm
+    .include src/gfx_effects/video_updates/star_firework/bottom_main.asm
+    .include src/gfx_effects/video_updates/star_firework/bottom_secondary.asm
