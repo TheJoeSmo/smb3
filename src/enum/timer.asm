40d696623c73eb1c20651a3e47f71f8fd3cd1a4e
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-12-21 05:06:54 -0500

    Create timer enumeration

---
+++ src/enum/timer.asm
@@ -0,0 +1,10 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Timer Enumeration
+; Joe Smo 12/21/2020
+; Includes for all enumeration for the timer
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+level_timer_count 			= Level_TimerTick
+level_time_lo 				= Level_TimerLSD
+level_time_mi 				= Level_TimerMid
+level_time_hi 				= Level_TimerMSD
